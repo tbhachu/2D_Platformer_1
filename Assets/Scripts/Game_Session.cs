c0c26f041f5b718f706cc385c45bbcdc215d2f8d
tbhachu <sem.b1901@gmail.com>
2022-12-22 14:48:36 -0800

    Added level exit portals to all levels

---
+++ Assets/Scripts/Game_Session.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Game_Session : MonoBehaviour
+{
+    [SerializeField] int playerLives = 3;
+    
+    void Awake()
+    {
+        int numGameSessions = FindObjectsOfType<GameSession>().Length;
+        if (numGameSessions > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            {
+                DontDestroyOnLoad(gameObject);
+            }
+        }
+    }
+
+    public void ProcessPlayerDeath()
+    {
+        if (playerLives > 1){
+            TakeLife();
+        } else {
+            ResetGameSession();
+        }
+    }
+
+    private void ResetGameSession()
+    {
+        SceneManager
+    }
+}
